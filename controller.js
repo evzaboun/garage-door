7a0fb853d2b73c78c034466fcd1dd46942d03647
vagelis z <29361890+evzab@users.noreply.github.com>
2020-02-20 23:52:50 +0200

    added reed switches logic

--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
 // Garage signal decision controller
 //+++ Switch logic by type+++//
-// Reed switch: If reed is closed, relay should wait 3 seconds before disabling itself. The door has been fully closed or oppened, and the relay disables itself to save energy.
+// Reed switch: If reed is closed, relay should wait 5 seconds before disabling itself. The door has been fully closed or oppened, and the relay disables itself to save energy.
 // PushButton switch: Physical push button should open/close the door for the time pressed
 // UIButton: App button, when pushed momentarily it opens/closes/freezes the door
 
@@ -8,6 +8,10 @@ const Gpio = require("onoff").Gpio;
 const eventEmitter = require("./eventEmitter");
 const openButton = new Gpio(6, "in", "both", { debounceTimeout: 100 });
 const closeButton = new Gpio(5, "in", "both", { debounceTimeout: 100 });
+const reedSwitchTop = new Gpio(23, "in", "falling", { debounceTimeout: 100 });
+const reedSwitchBottom = new Gpio(24, "in", "falling", {
+  debounceTimeout: 100
+});
 const relayClose = new Gpio(27, "high");
 const relayOpen = new Gpio(22, "high");
 let state = 0;
@@ -16,12 +20,33 @@ eventEmitter.on("open", event => direction(event));
 eventEmitter.on("close", event => direction(event));
 eventEmitter.on("freeze", event => direction(event));
 
+reedSwitchTop.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+  if (value === 0) {
+    setTimeout(() => eventEmitter.emit("freeze", "freeze"), 5000);
+  }
+  console.log(`Top Reed Switch activated! value: ${value}`);
+});
+
+reedSwitchBottom.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+  if (value === 0) {
+    setTimeout(() => eventEmitter.emit("freeze", "freeze"), 5000);
+  }
+  console.log(`Bottom Reed Switch activated! value: ${value}`);
+});
+
 openButton.watch((err, value) => {
   if (err) {
     throw err;
   }
   if (value === 0) {
     eventEmitter.emit("open", "open");
+    console.log(`Open push button pressed`);
   }
 
   if (value === 1) {
@@ -35,6 +60,7 @@ closeButton.watch((err, value) => {
   }
   if (value === 0) {
     eventEmitter.emit("close", "close");
+    console.log(`Close push button pressed`);
   }
 
   if (value === 1) {
